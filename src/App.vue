e327303c54b01be9e551e771cd711e8904fe5b7e
Wallesu <wallysonsouza4@hotmail.com>
2022-03-24 23:09:07 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-    <div id="app">
-        <Map msg="Olá" />
-    </div>
+	<div id="app">
+		<div class="row m-0">
+			<div class="col-4 m-0 p-0">
+				<SideBar></SideBar>
+			</div>
+			<div class="col-4 m-0 p-0">
+				<Map />
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Map from './components/Map.vue'
+import Map from './components/Map.vue';
+import SideBar from './components/SideBar.vue';
 
 export default {
-    name: 'App',
-    components: {
-        Map,
-    },
-}
+	name: 'App',
+	components: {
+		Map,
+		SideBar,
+	},
+};
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: #2c3e50;
 }
+body {
+	background-color: #bde4e5;
+}
+@import '~bootstrap/dist/css/bootstrap.css';
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<div class="bar bg-danger"></div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'SideBar',
+};
+</script>
+<style scoped>
+.bar {
+	width: 20rem;
+	height: 100vh;
+	/* background-color: black; */
+}
+</style>
