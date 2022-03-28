40cedf795795da3b1c04028704d25e7d8dd5df10
Wallesu <wallysonsouza4@hotmail.com>
2022-03-27 23:16:00 -0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,51 @@
 <template>
-    <div id="app">
-        <div class="row m-0">
-            <div class="col-3 m-0 p-0">
-                <SideBar></SideBar>
-            </div>
-            <div class="col-9 m-0 p-0">
-                <div class="row m-0">
-                    <h1>Mapa de Joinville</h1>
-                </div>
-                <div class="row m-0">
-                    <Map />
-                </div>
-            </div>
-        </div>
-    </div>
+	<div id="app">
+		<div class="row m-0">
+			<div class="col-3 m-0 p-0">
+				<SideBar :districtInfo="districtInfo"></SideBar>
+			</div>
+			<div class="col-9 m-0 p-0">
+				<div class="row m-0">
+					<Map @sendDistrictInfo="getDistrictInfo"></Map>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import Map from './components/Map.vue'
-import SideBar from './components/SideBar.vue'
+import Map from './components/Map.vue';
+import SideBar from './components/SideBar.vue';
 
 export default {
-    name: 'App',
-    components: {
-        Map,
-        SideBar,
-    },
-}
+	name: 'App',
+	components: {
+		Map,
+		SideBar,
+	},
+	data() {
+		return {
+			districtInfo: '',
+		};
+	},
+	methods: {
+		getDistrictInfo(value) {
+			this.districtInfo = value;
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	color: #2c3e50;
 }
 h1 {
 }
 body {
-    background-color: #bde4e5;
+	background-color: #bde4e5;
 }
+
 @import '~bootstrap/dist/css/bootstrap.css';
 </style>
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,21 +1,49 @@
 <template>
-    <div>
-        <div class="bar bg-danger">
-            <ul>
-                <li>Mapa de Joinville</li>
-            </ul>
-        </div>
-    </div>
+	<div>
+		<div class="bar bg-danger">
+			<h2 class="pt-3">Mapa de Joinville</h2>
+			<h3 v-if="!districtInfo">Selecione um bairro</h3>
+			<ul v-else class="list mt-4">
+				<li class="districtTitle pb-2">{{ districtInfo.name }}</li>
+				<li>População: {{ districtInfo.population }}</li>
+				<li>Densidade: {{ districtInfo.density }} hab/km²</li>
+				<li>Área: {{ districtInfo.area }} km²</li>
+			</ul>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-    name: 'SideBar',
-}
+	name: 'SideBar',
+	props: {
+		districtInfo: {
+			type: Object,
+		},
+	},
+};
 </script>
 <style scoped>
 .bar {
-    width: 100%;
-    height: 100vh;
-    /* background-color: black; */
+	width: 100%;
+	height: 100vh;
+	/* background-color: black; */
+}
+h2 {
+	font-size: 2.5rem;
+	font-weight: bold;
+	color: white;
+	text-align: center;
+}
+h3 {
+	text-align: center;
+	font-weight: bold;
+}
+.list {
+	font-size: 1.5rem;
+	list-style: none;
+}
+.districtTitle {
+	font-weight: bold;
+	font-size: 1.7rem;
 }
 </style>
